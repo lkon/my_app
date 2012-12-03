00ebfe462ab012ef3ca1197ad6285fa91bae1f13
ekonina <ekonina@ipetrov.users.rbc.ru>
2012-12-03 15:55:08 +0400

    Обработка загрузки файлов - 1 шаг

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 var http = require("http");
 var url = require("url");
 
-function start( route, handle ) {
+function start(route, handle) {
 
-	function onRequest( request, response ) {
+  function onRequest(request, response) {
+    var postData = "";
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
 
-	  var pathname = url.parse( request.url ).pathname;
+    request.setEncoding("utf8");
 
-	  console.log( "Request for " + pathname + ' received.' );
+    request.addListener("data", function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '" + postDataChunk + "'.");
+    });
 
-	  var content = route(handle, pathname, response);
-	}
+    request.addListener("end", function() {
+      route(handle, pathname, response, postData);
+    });
 
-	http.createServer( onRequest ).listen( 8888 );
+  }
 
-	console.log("Server has started.");
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
 exports.start = start;
